10e08af9e124db79ca1e15c7340c814f78ccdb3d
Bryant-Anjos <bryant_mx@hotmail.com>
2020-01-01 15:36:37 -0300

    CheckIns page done

--- src/store/modules/rootReducer.js
+++ src/store/modules/rootReducer.js
@@ -3,9 +3,11 @@ import { combineReducers } from 'redux'
 import auth from './auth/reducer'
 import user from './user/reducer'
 import questions from './questions/reducer'
+import checkins from './checkins/reducer'
 
 export default combineReducers({
   auth,
   user,
   questions,
+  checkins,
 })
--- src/store/modules/rootSaga.js
+++ src/store/modules/rootSaga.js
@@ -3,7 +3,8 @@ import { all } from 'redux-saga/effects'
 import auth from './auth/sagas'
 import user from './user/sagas'
 import questions from './questions/sagas'
+import checkins from './checkins/sagas'
 
 export default function* rootSaga() {
-  return yield all([auth, user, questions])
+  return yield all([auth, user, questions, checkins])
 }
